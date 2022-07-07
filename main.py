e47b4a92adcd63e1036c451eb1ef1cd6eb3d2ba9
Mario Strachan <96462826+Rio242@users.noreply.github.com>
2022-07-07 14:46:37 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,16 @@
+# 🚨 Don't change the code below 👇
+student_heights = input("Input a list of student heights ").split()
+for n in range(0, len(student_heights)):
+  student_heights[n] = int(student_heights[n])
+# 🚨 Don't change the code above 👆
+print(student_heights)
+total_height = 0
+for height in student_heights:
+  total_height += height
+print(total_height)
+#Write your code below this row 👇
+
+
+average = total_height/len(student_heights)
+
+print(average)
